ca2b0313096c8cd278928436eabaf0c44838cb46
qqtc0 <narcotics@startmail.com>
2022-04-05 16:46:11 +0200

    Begin porting to Avalonia

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Vival
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
