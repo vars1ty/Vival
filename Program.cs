b308033313698fc030619fd4b55379afc3c2a249
qqtc0 <narcotics@startmail.com>
2022-04-07 17:16:25 +0200

    Add back toggle behavior

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Avalonia;
 
 namespace Vival;
@@ -9,7 +10,27 @@ internal static class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // ? If Vival was already opened, close it. Acting as a toggle behavior.
+        if (Process.GetProcessesByName("Vival") is not {Length: 1})
+        {
+            KillVival();
+            return;
+        }
+
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
+
+    /// <summary>
+    /// Kills all Vival instances.
+    /// </summary>
+    private static void KillVival()
+    {
+        var proc = Process.GetProcessesByName("Vival");
+        for (var i = 0; i < proc.Length; i++) proc[i].Kill();
+        Environment.Exit(0);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
